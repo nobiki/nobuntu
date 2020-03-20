67426fe9fcf7cdcf3d438b749ea304896c5ccaf4
Naoaki Obiki <obiki.wk@gmail.com>
2020-03-20 21:56:41 +0900

    new file:   shell/git_find_big.sh shell/hadolint.sh

---
+++ shell/hadolint.sh
@@ -0,0 +1,4 @@
+sudo wget "https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64" -O /usr/local/bin/hadolint-Linux-x86_64
+sudo mv /usr/local/bin/hadolint-Linux-x86_64 /usr/local/bin/hadolint
+sudo chmod +x /usr/local/bin/hadolint
+
