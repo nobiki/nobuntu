ef6465edf4760d7ac343624cf45b27c625706a90
Naoaki Obiki <obiki.wk@gmail.com>
2020-03-20 22:38:21 +0900

    modified:   provision.sh

--- provision.sh
+++ provision.sh
@@ -1,10 +1,52 @@
 ./shell/apt/core.sh
 ./shell/apt/common.sh
 ./shell/apt/development.sh
+
 ./shell/unity-desktop/tweaks.sh*
 ./shell/unity-desktop/font.sh*
 ./shell/unity-desktop/icon.sh*
 ./shell/unity-desktop/theme.sh*
 
+mkdir -p ~/.config/terminator
+wget -O ~/.config/terminator/config https://raw.githubusercontent.com/nobiki/nobian/master/config/gnome-desktop/terminator.config
+
+./shell/docker-ce.sh
+./shell/certbot.sh
+
 ./shell/anyenv.sh
 ./shell/direnv.sh
+# ./shell/nodenv.sh
+# ./shell/phpenv.sh
+# ./shell/pyenv.sh
+# ./shell/rbenv.sh
+
+./shell/fzf.sh
+./shell/bat.sh
+./shell/exa.sh
+./shell/hcat.sh
+./shell/jq.sh
+./shell/highway.sh
+./shell/ripgrep.sh
+./shell/trans.sh
+./shell/stacer.sh
+./shell/sql-formatter.sh
+./shell/xpanes.sh
+
+./shell/ctags.sh
+./shell/js-beautify.sh
+./shell/phpcs.sh
+./shell/rsense.sh
+./shell/hadolint.sh
+./shell/git_find_big.sh
+
+./shell/helm.sh
+
+./shell/nginx.sh
+./shell/ngrok.sh
+
+./shell/chrome.sh
+./shell/jmeter.sh
+./shell/postman.sh
+
+./shell/awscli.sh
+./shell/gcloud.sh
