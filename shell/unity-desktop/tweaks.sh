1c89f5e83fa50a300d5cc16a0ac5e52f7fcbd9ed
Naoaki Obiki <obiki.wk@gmail.com>
2020-03-22 00:41:54 +0900

    modified:   shell/unity-desktop/tweaks.sh

--- shell/unity-desktop/tweaks.sh
+++ shell/unity-desktop/tweaks.sh
@@ -1,1 +1,8 @@
-sudo apt install -y gnome-tweaks
+sudo apt install -y \
+       	gnome-tweaks \
+        gnome-shell-extension-dashtodock \
+        gnome-shell-extension-autohidetopbar \
+        gnome-shell-extension-system-monitor \
+        gnome-shell-extension-hard-disk-led \
+        gnome-shell-extension-log-out-button \
+        gnome-shell-extensions-gpaste
