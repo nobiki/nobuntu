ffb596d23686dab89fc31fee76d655e0de7782d2
Naoaki Obiki <obiki.wk@gmail.com>
2020-03-22 13:06:44 +0900

    fix path

--- shell/jmeter.sh
+++ shell/jmeter.sh
@@ -4,6 +4,6 @@ sudo mkdir -p /usr/local/src/jmeter/
 sudo wget "http://ftp.riken.jp/net/apache/jmeter/binaries/apache-jmeter-${JMETER_VER}.tgz" -O /usr/local/src/jmeter/apache-jmeter-${JMETER_VER}.tgz
 sudo tar zxvf /usr/local/src/jmeter/apache-jmeter-${JMETER_VER}.tgz -C /usr/local/lib/
 sudo chown -R 1000 /usr/local/lib/apache-jmeter-${JMETER_VER}/
-echo "PATH=${PATH}:/usr/local/lib/apache-jmeter-${JMETER_VER}/bin/" >> ~/.bash_profile
+echo 'PATH=${PATH}:/usr/local/lib/apache-jmeter-'${JMETER_VER}'/bin/' >> ~/.bash_profile
 echo 'export JVM_ARGS="-Duser.language=ja -Dawt.useSystemAAFontSettings=lcd"' >> ~/.bash_profile
 sudo rm -f /usr/local/src/jmeter/apache-jmeter-${JMETER_VER}.tgz
