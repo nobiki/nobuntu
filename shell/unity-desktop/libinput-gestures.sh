345463bdb721f58ca1cd39da22ffb1beeddab697
Naoaki Obiki <obiki.wk@gmail.com>
2020-03-25 14:11:05 +0900

    libinput-gestures

--- shell/unity-desktop/libinput-gestures.sh
+++ shell/unity-desktop/libinput-gestures.sh
@@ -2,6 +2,7 @@ sudo apt install -y xdotool wmctrl libinput-tools
 
 git clone git@github.com:bulletmark/libinput-gestures
 cd libinput-gestures/ && sudo make install
+rm -rf libinput-gestures
 
 sudo gpasswd -a ${USER} input
 libinput-gestures-setup autostart
